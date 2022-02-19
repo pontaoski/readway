13e8397e638a50e3c70c9a7bf533f5c5fc864f5a
Jan Blackquill <uhhadd@gmail.com>
2022-02-19 17:38:33 -0500

    fix wasm

--- app/renderer.go
+++ app/renderer.go
@@ -10,29 +10,31 @@ import (
 	"github.com/qor/render"
 )
 
+var funcMap = template.FuncMap{
+	"unescape": func(in string) template.HTML {
+		return template.HTML(in)
+	},
+	"isLast": func(index, len int) bool {
+		return index+1 == len
+	},
+	"hasField": func(v interface{}, name string) bool {
+		rv := reflect.ValueOf(v)
+		if rv.Kind() == reflect.Ptr {
+			rv = rv.Elem()
+		}
+		if rv.Kind() != reflect.Struct {
+			return false
+		}
+		return rv.FieldByName(name).IsValid()
+	},
+	"join_strings": strings.Join,
+}
+
 var renderer = render.New(&render.Config{
 	ViewPaths:     []string{"app/templates"},
 	DefaultLayout: "application",
 	FuncMapMaker: func(*render.Render, *http.Request, http.ResponseWriter) template.FuncMap {
-		return map[string]interface{}{
-			"unescape": func(in string) template.HTML {
-				return template.HTML(in)
-			},
-			"isLast": func(index, len int) bool {
-				return index+1 == len
-			},
-			"hasField": func(v interface{}, name string) bool {
-				rv := reflect.ValueOf(v)
-				if rv.Kind() == reflect.Ptr {
-					rv = rv.Elem()
-				}
-				if rv.Kind() != reflect.Struct {
-					return false
-				}
-				return rv.FieldByName(name).IsValid()
-			},
-			"join_strings": strings.Join,
-		}
+		return funcMap
 	},
 })
 
--- app/wasm.go
+++ app/wasm.go
@@ -1,3 +1,4 @@
+//go:build wasm
 // +build wasm
 
 package app
@@ -15,16 +16,8 @@ func WASMRender(data string) string {
 	if err != nil {
 		return (err.Error())
 	}
-	tmpl, err := template.New("").Parse(`
+	tmpl, err := template.New("").Funcs(funcMap).Parse(`
 	{{ define "description" }}
-	{{ if . }}
-		<p class="lead">
-			{{ . }}
-		</p>
-	{{ end }}
-	{{ end }}
-	
-	{{ define "description-light" }}
 	{{ if . }}
 		<p>
 			{{ . }}
@@ -44,18 +37,108 @@ func WASMRender(data string) string {
 	{{ end }}
 	{{ end }}
 	
-	{{ define "arguments" }}
-	{{ if . }}
-		<div class="list-group list-group-flush">
+	{{ define "arguments-table" }}
+	{{ with . }}
+	<h5 class="font-bold">Arguments</h5>
+	<table>
+		<thead>
+			<tr>
+				<th>
+					Name
+				</th>
+				<th>
+					Type
+				</th>
+				<th>
+					Description
+				</th>
+			</tr>
+		</thead>
+		<tbody>
 			{{ range $arg := . }}
-				<span class="list-group-item">
-					<h6 class="mb-1">{{ $arg.Name }} {{ template "()-light" $arg.Type }} {{ template "()-light" $arg.Interface }} {{ template "()-light" $arg.Enum }}</h6>
-					{{ template "description-light" $arg.Summary }}
-					{{ template "description-light" $arg.Description.Body }}
-				</span>
+			<tr>
+				<td>{{ $arg.Name }}</td>
+				<td>{{ $arg.Type }}{{- with $arg.Interface -}}[{{ . }}]{{ end }}</td>
+				<td>{{ $arg.Summary }}</td>
+			</tr>
 			{{ end }}
-		</div>
+		</tbody>
+	</table>
 	{{ end }}
+	{{ end }}
+	
+	{{ define "enum-table" }}
+	{{ with . }}
+	<h5 class="font-bold">Entries</h5>
+	<table>
+		<thead>
+			<tr>
+				<th>
+					Name
+				</th>
+				<th>
+					Value
+				</th>
+				<th>
+					Description
+				</th>
+			</tr>
+		</thead>
+		<tbody>
+			{{ range $enum := . }}
+			<tr>
+				<td>{{ $enum.Name }}</td>
+				<td>{{ $enum.Value }}</td>
+				<td>{{ $enum.Summary }}</td>
+			</tr>
+			{{ end }}
+		</tbody>
+	</table>
+	{{ end }}
+	{{ end }}
+	
+	{{ define "arguments" }}
+	{{- if . -}}
+		{{- $alen := len . -}}
+		{{- range $idx, $arg := . -}}
+			{{- $arg.Name }} <span class="symbol-structure">{{$arg.Type}}</span>
+			{{- with $arg.Interface -}}
+				[<span class="symbol-class">{{ . }}</span>]
+			{{- end -}}
+			{{- if not (isLast $idx $alen) }}, {{ end -}}
+		{{ end -}}
+	{{- end -}}
+	{{ end }}
+	
+	{{ define "name" -}}
+	{{- $req := . -}}
+	{{- if hasField $req "Type" -}}
+	{{- if eq $req.Type "destructor" -}}
+		~
+	{{- end -}}
+	{{- end -}}
+	<span class="symbol-method">{{- $req.Name -}}</span>
+	{{- end }}
+	
+	{{ define "message-name" }}
+	{{ $msg := . }}
+	{{ $msg.Name }}
+	{{ if hasField $msg "Type" }}
+	{{ template "()-light" $msg.Type }}
+	{{ end }}
+	{{ end }}
+	
+	{{ define "message" }}
+	{{ $msg := . }}
+	<h4 id="{{ $msg.Anchor }}">{{ template "message-name" $msg }} <small class="text-muted">since version {{ $msg.Since }}</small></h4>
+	<pre><code>{{ template "name" $msg }}({{ template "arguments" $msg.Arguments }})</code></pre>
+	<div>
+		<div>
+			{{ template "description" $msg.Description.Body }}
+			{{ template "arguments-table" $msg.Arguments }}
+		</div>
+	</div>
+	<br>
 	{{ end }}
 	
 	{{ define "requests" }}
@@ -63,17 +146,7 @@ func WASMRender(data string) string {
 		<h3>Requests</h3>
 		<div>
 		{{ range $req := . }}
-			<div class="card {{ if eq $req.Type "destructor" }}text-white bg-danger{{ end }}" id="{{ $req.Anchor }}">
-				<div class="card-header">
-					{{ $req.Name }} {{ template "()-light" $req.Type }} <small class="text-muted">since version {{ $req.Since }}</small>
-				</div>
-				<div class="card-body">
-					{{ template "description-light" $req.Description.Body }}
-					{{ if $req.Arguments }}<h5>Arguments</h5>{{ end }}
-				</div>
-				{{ template "arguments" $req.Arguments }}
-			</div>
-			<br>
+		{{ template "message" $req }}
 		{{ end }}
 		</div>
 	{{ end }}
@@ -83,32 +156,17 @@ func WASMRender(data string) string {
 	{{ if . }}
 		<h3>Events</h3>
 		<div>
-		{{ range $ev := . }}
-			<div class="card" id="{{ $ev.Anchor }}">
-				<div class="card-header">
-					{{ $ev.Name }} <small class="text-muted">since version {{ $ev.Since }}</small>
-				</div>
-				<div class="card-body">
-					{{ template "description-light" $ev.Description.Body }}
-				</div>
-				{{ template "arguments" $ev.Arguments }}
-			</div>
-			<br>
+		{{ range $req := . }}
+		{{ template "message" $req }}
 		{{ end }}
 		</div>
 	{{ end }}
 	{{ end }}
 	
 	{{ define "enum-entries" }}
-	{{ if . }}
-		{{ range $entry := . }}
-		<ul class="list-group list-group-flush">
-			<li class="list-group-item">
-				{{ $entry.Name }} ({{ $entry.Value }}) <small class="text-muted">since version {{ $entry.Since }}</small> <br>
-				{{ $entry.Summary }}
-			</li>
-		</ul>
-		{{ end }}
+	{{ $enum := . }}
+	{{ with $enum.Entries }}
+		<pre><code><span class="symbol-keyword">enum</span> <span class="symbol-enum">{{ $enum.Name }}</span> {<br/>{{ range $entry := . }}    <span class="symbol-enum-member">{{ $entry.Name }}</span> = {{ $entry.Value -}},<br>{{ end }}}</code></pre>
 	{{ end }}
 	{{ end }}
 	
@@ -117,17 +175,12 @@ func WASMRender(data string) string {
 		<h3>Enums</h3>
 		<div>
 		{{ range $enum := . }}
-			<div class="card" id="{{ $enum.Anchor }}">
-				<div class="card-header">
-					{{ if $enum.Bitfield }}Flagset{{ end }} {{ $enum.Name }} <small class="text-muted">since version {{ $enum.Since }}</small>
-				</div>
-				<div class="card-body">
-					{{ template "description-light" $enum.Description.Body }}
-					{{ if $enum.Entries }}<h5>Entries</h5>{{ end }}
-				</div>
-				{{ template "enum-entries" $enum.Entries }}
+			<div id="{{ $enum.Anchor }}">
+				<h4>{{ if $enum.Bitfield }}Flagset{{ end }} {{ $enum.Name }} <small class="text-muted">since version {{ $enum.Since }}</small></h4>
+				{{ template "description" $enum.Description.Body }}
+				{{ template "enum-entries" $enum }}
 			</div>
-			<br>
+			{{ template "enum-table" $enum.Entries }}
 		{{ end }}
 		</div>
 	{{ end }}
@@ -142,110 +195,71 @@ func WASMRender(data string) string {
 			<meta property="og:description" content="Why read XML when you can use a website with well-designed typography?" />
 			<meta name="viewport" content="width=device-width, initial-scale=1">
 			<script src="wasm_exec.js"></script>
-			<link rel="stylesheet" type="text/css" href="https://cdn.kde.org/aether-devel/bootstrap.css" />
-			<link rel="stylesheet" type="text/css" href="https://cdn.kde.org/aether-devel/aether-sidebar.css" />
-			<style>
-				.list-group-item {
-					background-color: var(--base);
-				}
-				.text-white {
-					color: white !important;
-				}
-				.text-white .text-muted {
-					color: rgba(255,255,255,0.5) !important;
-				}
-				.bg-danger {
-					background-color: #dc3545 !important;
-				}
-				@media (prefers-color-scheme: dark) {
-					.menu-title h2 {
-						color: #eff0f1 !important;
-					}
-				}
-			</style>
+			<link rel="stylesheet" href="main.css"/>
+			<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@vscode/codicons@0.0.28/dist/codicon.css" />
 		</head>
-		<body>
-			<main class="body">				
-				<header class="header">
-					<nav class="navbar">
-						<ol class="breadcrumb navbar-nav">
-							<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault"
-								aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-								<span class="navbar-toggler-icon"></span>
-							</button>
-							<li class="breadcrumb-link">
-								<a href="/">ReadWay Protocol Browser</a>
-							</li>
-							<li class="breadcrumb-link">
-								<a href="#">{{ .Name }}</a>
-							</li>
-						</ol>
+		<body class="bg-stone-100 dark:bg-stone-900 text-black dark:text-white">
+			<main>
+				<header class="bg-stone-200 dark:bg-stone-800 p-2">
+					<nav class="flex flex-row space-x-2">
+						<a href="index.html">ReadWay Protocol Browser</a>
+						<span> / </span>
+						<a href="#">{{ .Name }}</a>
 					</nav>
 				</header>
 				
-				<aside class="sidebar">
-					<div id="sidebar-header" class="menu-box">
-						<div class="menu-title">
-							<h2>ReadWay</h2>
-						</div>
-					</div>
-					<div class="menu-box">
-						<ul>
+				<div class="flex flex-row w-full">
+					<aside class="prose dark:prose-invert prose-sm w-full max-w-xs p-4">
 						{{ range $iface := .Interfaces }}
-							<li>
-								<h4><a href="#{{ $iface.Anchor }}">{{ $iface.Name }}</a><br></h4>
-								{{ if $iface.Requests }}
-								Requests:
-								<ul>
-								{{ range $req := $iface.Requests }}
-									<li><a href="#{{ $req.Anchor }}">{{ $req.Name }}</a></li>
-								{{ end }}
-								</ul>
-								{{ end }}
-								{{ if $iface.Events }}
-								Events:
-								<ul>
-								{{ range $ev := $iface.Events }}
-									<li><a href="#{{ $ev.Anchor }}">{{ $ev.Name }}</a></li>
-								{{ end }}
-								</ul>
-								{{ end }}
-								{{ if $iface.Enums }}
-								Enums:
-								<ul>
-								{{ range $en := $iface.Enums }}
-									<li><a href="#{{ $en.Anchor }}">{{ $en.Name }}</a></li>
-								{{ end }}
-								</ul>
-								{{ end }}
-								<br>
-							</li>
+							<h4><span class="codicon codicon-symbol-class symbol-class"></span><a href="#{{ $iface.Anchor }}">{{ $iface.Name }}</a><br></h4>
+							{{ if $iface.Requests }}
+							Requests:
+							<ul>
+							{{ range $req := $iface.Requests }}
+								<li><span class="codicon codicon-symbol-method symbol-method"></span><a href="#{{ $req.Anchor }}">{{ $req.Name }}</a></li>
+							{{ end }}
+							</ul>
+							{{ end }}
+							{{ if $iface.Events }}
+							Events:
+							<ul>
+							{{ range $ev := $iface.Events }}
+								<li><span class="codicon codicon-symbol-event symbol-event"></span><a href="#{{ $ev.Anchor }}">{{ $ev.Name }}</a></li>
+							{{ end }}
+							</ul>
+							{{ end }}
+							{{ if $iface.Enums }}
+							Enums:
+							<ul>
+							{{ range $en := $iface.Enums }}
+								<li><span class="codicon codicon-symbol-enum symbol-enum"></span><a href="#{{ $en.Anchor }}">{{ $en.Name }}</a></li>
+							{{ end }}
+							</ul>
+							{{ end }}
 						{{ end }}
-						</ul>
-					</div>
-				</aside>
+					</aside>
+					<article class="prose dark:prose-invert w-full pt-4">
+						<h1>{{ .Name }} <small class="text-muted">protocol</small></h1>
 				
-				<article class="content">
-					<h1>{{ .Name }} <small class="text-muted">protocol</small></h1>
+						{{ template "description" .Description.Body }}
 				
-					{{ template "description" .Description.Body }}
+						{{ range $iface := .Interfaces }}
+							<h2 id="{{ $iface.Anchor }}">{{ $iface.Name }} <small class="text-muted">interface version {{ $iface.Version }}</small></h2>
 				
-					{{ range $iface := .Interfaces }}
-						<h2 id="{{ $iface.Anchor }}">{{ $iface.Name }} <small class="text-muted">interface version {{ $iface.Version }}</small></h2>
+							{{ template "description" $iface.Description.Body }}
 				
-						{{ template "description" $iface.Description.Body }}
+							{{ template "requests" $iface.Requests }}
+							
+							{{ template "events" $iface.Events }}
 				
-						{{ template "requests" $iface.Requests }}
-						
-						{{ template "events" $iface.Events }}
+							{{ template "enums" $iface.Enums }}
 				
-						{{ template "enums" $iface.Enums }}
+							<hr>
+						{{ end }}
 				
-						<br>
-						<hr>
-						<br>
-					{{ end }}
-				</article>
+						{{ template "description" .Copyright }}
+					</article>
+				</div>
 			</main>
 		</body>
 	</html>
